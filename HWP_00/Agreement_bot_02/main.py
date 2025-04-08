4ef9ade06695d2936f4cbef474d21c3921accd62
Muhammd Dawood Iqbal <dawoodjand356@gmail.com>
2025-04-09 01:15:43 +0500

    02 of 00_intro_python

---
+++ HWP_00/Agreement_bot_02/main.py
@@ -0,0 +1,8 @@
+print("Your Mimicry Bot 🤖")
+
+def main():
+    animal : str = input("Enter the name of your favorite animal: ")
+    print("My favorite animal is " + animal + " too!")
+
+if __name__ == "__main__":
+    main()

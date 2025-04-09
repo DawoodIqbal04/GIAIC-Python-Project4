fce6b27ee6973c9be1a0e54b1dc439fac97ce681
Muhammd Dawood Iqbal <dawoodjand356@gmail.com>
2025-04-09 16:02:32 +0500

    05 of 00_intro_python

---
+++ HWP_00/Triangle_perimeter_05/main.py
@@ -0,0 +1,10 @@
+
+def main():
+    side1 : float = float(input("Enter the length of the first side of triangle: "))
+    side2 : float = float(input("Enter the length of the second side of triangle: "))
+    side3 : float = float(input("Enter the length of the third side of triangle: "))
+
+    print(f"The Perimeter of the triangle is {str(side1 + side2 + side3)}")
+
+if __name__ == "__main__":
+    main()
